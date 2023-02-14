2c8be1aa64b4c12c5fa7caecb0ac832cea4ff6fd
pappy <pappy@DESKTOP-4IE01BQ.lan>
2023-02-14 11:25:17 -0800

    First commit....

---
+++ SimpleMavenTest/src/test/java/TestCases/TC_BaseClass.java
@@ -0,0 +1,25 @@
+package TestCases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TC_BaseClass {
+	
+	@Test
+	public void myMethod()
+	{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.get("https://demo.guru99.com/v4/");
+
+	}
+
+}
